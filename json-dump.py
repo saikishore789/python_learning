44e37a594714f365fd47e2312a36127509afcc63
sai kishore reddy  Rachumalla <saikishorereddy789@gmail.com>
2023-12-09 11:40:33 +0000

    json dump examples

---
+++ json-dump.py
@@ -0,0 +1,26 @@
+import json
+
+dict1 ={ 
+    "emp1": { 
+        "name": "Lisa", 
+        "designation": "programmer", 
+        "age": "34", 
+        "salary": "54000"
+    }, 
+    "emp2": { 
+        "name": "Elis", 
+        "designation": "Trainee", 
+        "age": "24", 
+        "salary": "40000"
+    }, 
+} 
+  
+# the json file where the output must be stored 
+#out_file = open("myfile.json", "w") 
+  
+#json.dump(dict1, out_file, indent = 6) 
+  
+#out_file.close() 
+
+with open("myfile.json", "w") as json_file:
+    json.dump(dict1, json_file, indent = 1)
--- update-server.py
+++ update-server.py
@@ -5,12 +5,12 @@ def update_server_config(file_path, key, value):
     with open(file_path, 'w') as file:
         for line in lines:
             if key in line:
-                file.write(key + "=" + value + "\n")
+                file.write(key + " " + "=" + " " + value + "\n")
             else:
                 file.write(line)
 
 server_config_file = 'server.conf'
-key_to_update = 'MAX_CONNECTIONS'
-new_value = '600' 
+key_to_update = 'SSL_ENABLED'
+new_value = 'false' 
 
 update_server_config(server_config_file, key_to_update, new_value)
